6e20d87c08fc82d87cc1cfaa17b34751ec3c733a
maoxiangyi <maoxiangyi@aliyun.com>
2024-04-18 14:39:37 +0800

    Update data_process.py
    修改路径

--- data_process.py
+++ data_process.py
@@ -226,53 +226,53 @@ if __name__=="__main__":
     # process_medical('./data/shibing624_medical/pretrain/train_encyclopedia.json','encyclopedia')
     process_baidu()
     process_c4()
-    # process_wudao()
+    process_wudao()
 
     # print('data processing finished!')
 
-    # 分词处理后的文件列表
-    # data_path_list=[
-    #     './data/baidubaike_563w_1.bin',
-    #     './data/baidubaike_563w_2.bin',
-    #     './data/baidubaike_563w_3.bin',
-    #     './data/baidubaike_563w_4.bin',
-    #     './data/baidubaike_563w_5.bin',
-    #     './data/medical_book.bin',
-    #     './data/medical_encyclopedia.bin',
-    #     './data/wiki.bin',
-    #     './data/c4_zh_0.bin',
-    #     './data/c4_zh_1.bin',
-    #     './data/c4_zh_2.bin',
-    #     './data/c4_zh_3.bin',
-    #     './data/c4_zh_4.bin',
-    #     './data/c4_zh_5.bin',
-    #     './data/c4_zh_6.bin',
-    #     './data/c4_zh_7.bin',
-    #     './data/c4_zh_8.bin',
-    #     './data/wudaocorpus_zh_0.bin',
-    #     './data/wudaocorpus_zh_1.bin',
-    #     './data/wudaocorpus_zh_2.bin',
-    #     './data/wudaocorpus_zh_3.bin',
-    #     './data/wudaocorpus_zh_4.bin',
-    #     './data/wudaocorpus_zh_5.bin',
-    #     './data/wudaocorpus_zh_6.bin',
-    #     './data/wudaocorpus_zh_7.bin',
-    #     './data/wudaocorpus_zh_8.bin',
-    #     './data/wudaocorpus_zh_9.bin',
-    #     './data/wudaocorpus_zh_10.bin',
-    #     './data/wudaocorpus_zh_11.bin',
-    #     './data/wudaocorpus_zh_12.bin',
-    #     './data/wudaocorpus_zh_13.bin',
-    #     './data/wudaocorpus_zh_14.bin',
-    #     './data/wudaocorpus_zh_15.bin',
-    #     './data/wudaocorpus_zh_16.bin',
-    # ]
-    # data_lst=[]
-    # for data_path in tqdm(data_path_list):
-    #     with open(data_path,'rb') as f:
-    #         data=np.fromfile(f,dtype=np.uint16)
-    #         data_lst.append(data)
-    # arr = np.concatenate(data_lst)
-    # print(arr.shape)
-    # with open('./data/pretrain_data.bin','wb') as f:
-    #     f.write(arr.tobytes())
+    分词处理后的文件列表
+    data_path_list=[
+        './data/baidubaike_563w_1.bin',
+        './data/baidubaike_563w_2.bin',
+        './data/baidubaike_563w_3.bin',
+        './data/baidubaike_563w_4.bin',
+        './data/baidubaike_563w_5.bin',
+        './data/medical_book.bin',
+        './data/medical_encyclopedia.bin',
+        './data/wiki.bin',
+        './data/c4_zh_0.bin',
+        './data/c4_zh_1.bin',
+        './data/c4_zh_2.bin',
+        './data/c4_zh_3.bin',
+        './data/c4_zh_4.bin',
+        './data/c4_zh_5.bin',
+        './data/c4_zh_6.bin',
+        './data/c4_zh_7.bin',
+        './data/c4_zh_8.bin',
+        './data/wudaocorpus_zh_0.bin',
+        './data/wudaocorpus_zh_1.bin',
+        './data/wudaocorpus_zh_2.bin',
+        './data/wudaocorpus_zh_3.bin',
+        './data/wudaocorpus_zh_4.bin',
+        './data/wudaocorpus_zh_5.bin',
+        './data/wudaocorpus_zh_6.bin',
+        './data/wudaocorpus_zh_7.bin',
+        './data/wudaocorpus_zh_8.bin',
+        './data/wudaocorpus_zh_9.bin',
+        './data/wudaocorpus_zh_10.bin',
+        './data/wudaocorpus_zh_11.bin',
+        './data/wudaocorpus_zh_12.bin',
+        './data/wudaocorpus_zh_13.bin',
+        './data/wudaocorpus_zh_14.bin',
+        './data/wudaocorpus_zh_15.bin',
+        './data/wudaocorpus_zh_16.bin',
+    ]
+    data_lst=[]
+    for data_path in tqdm(data_path_list):
+        with open(data_path,'rb') as f:
+            data=np.fromfile(f,dtype=np.uint16)
+            data_lst.append(data)
+    arr = np.concatenate(data_lst)
+    print(arr.shape)
+    with open('./data/pretrain_data.bin','wb') as f:
+        f.write(arr.tobytes())
