350172493f9a4ffb563b5ca6fc2bd2f5e337268d
maoxiangyi <maoxiangyi@aliyun.com>
2024-04-18 10:28:12 +0800

    Update data_process.py
    修改路径

--- data_process.py
+++ data_process.py
@@ -16,7 +16,7 @@ def process_wiki_clean():
         if len(text_id)>5:
             doc_ids+=text_id
     arr = np.array(doc_ids,dtype=np.uint16)
-    with open('./data/wiki.bin','wb') as f:
+    with open('/mnt/pfs/data_team/maoxiangyi/data/wiki.bin','wb') as f:
         f.write(arr.tobytes())
 
 def process_medical(data_path,name):
@@ -160,7 +160,7 @@ def process_baidu():
         batch_cnt+=1
         arr = np.array(doc_ids,dtype=np.uint16)
         print('cnt:',cnt,'arr_shape:',arr.shape)
-        with open('./data/baidubaike_563w_{}.bin'.format(batch_cnt),'wb') as f:
+        with open('/mnt/pfs/data_team/maoxiangyi/data/baidubaike_563w_{}.bin'.format(batch_cnt),'wb') as f:
             f.write(arr.tobytes())
     
 def process_c4():
@@ -182,7 +182,7 @@ def process_c4():
                 cnt+=1
 
     arr = np.array(doc_ids,dtype=np.uint16)
-    with open('./data/c4_zh.bin','wb') as f:
+    with open('/mnt/pfs/data_team/maoxiangyi/data/c4_zh.bin','wb') as f:
         f.write(arr.tobytes())
     print(arr.shape)
 
@@ -214,65 +214,65 @@ def process_wudao():
         #     f.write(arr.tobytes())
         # print(arr.shape)
     arr = np.array(doc_ids,dtype=np.uint16)
-    with open('./data/wudaocorpus_zh_16.bin','wb') as f:
+    with open('/mnt/pfs/data_team/maoxiangyi/data/wudaocorpus_zh_16.bin','wb') as f:
         f.write(arr.tobytes())
     print(arr.shape)
 
 if __name__=="__main__":
     tokenizer = ChatGLMTokenizer(vocab_file='./chatglm_tokenizer/tokenizer.model')
     # 数据预处理-如果下载分词处理后的数据，可以不用执行以下函数
-    # process_wiki_clean()
+    process_wiki_clean()
     # process_medical('./data/shibing624_medical/pretrain/medical_book_zh.json','book')
     # process_medical('./data/shibing624_medical/pretrain/train_encyclopedia.json','encyclopedia')
-    # process_baidu()
-    # process_c4()
+    process_baidu()
+    process_c4()
     # process_wudao()
 
     # print('data processing finished!')
 
     # 分词处理后的文件列表
-    data_path_list=[
-        './data/baidubaike_563w_1.bin',
-        './data/baidubaike_563w_2.bin',
-        './data/baidubaike_563w_3.bin',
-        './data/baidubaike_563w_4.bin',
-        './data/baidubaike_563w_5.bin',
-        './data/medical_book.bin',
-        './data/medical_encyclopedia.bin',
-        './data/wiki.bin',
-        './data/c4_zh_0.bin',
-        './data/c4_zh_1.bin',
-        './data/c4_zh_2.bin',
-        './data/c4_zh_3.bin',
-        './data/c4_zh_4.bin',
-        './data/c4_zh_5.bin',
-        './data/c4_zh_6.bin',
-        './data/c4_zh_7.bin',
-        './data/c4_zh_8.bin',
-        './data/wudaocorpus_zh_0.bin',
-        './data/wudaocorpus_zh_1.bin',
-        './data/wudaocorpus_zh_2.bin',
-        './data/wudaocorpus_zh_3.bin',
-        './data/wudaocorpus_zh_4.bin',
-        './data/wudaocorpus_zh_5.bin',
-        './data/wudaocorpus_zh_6.bin',
-        './data/wudaocorpus_zh_7.bin',
-        './data/wudaocorpus_zh_8.bin',
-        './data/wudaocorpus_zh_9.bin',
-        './data/wudaocorpus_zh_10.bin',
-        './data/wudaocorpus_zh_11.bin',
-        './data/wudaocorpus_zh_12.bin',
-        './data/wudaocorpus_zh_13.bin',
-        './data/wudaocorpus_zh_14.bin',
-        './data/wudaocorpus_zh_15.bin',
-        './data/wudaocorpus_zh_16.bin',
-    ]
-    data_lst=[]
-    for data_path in tqdm(data_path_list):
-        with open(data_path,'rb') as f:
-            data=np.fromfile(f,dtype=np.uint16)
-            data_lst.append(data)
-    arr = np.concatenate(data_lst)
-    print(arr.shape)
-    with open('./data/pretrain_data.bin','wb') as f:
-        f.write(arr.tobytes())
+    # data_path_list=[
+    #     './data/baidubaike_563w_1.bin',
+    #     './data/baidubaike_563w_2.bin',
+    #     './data/baidubaike_563w_3.bin',
+    #     './data/baidubaike_563w_4.bin',
+    #     './data/baidubaike_563w_5.bin',
+    #     './data/medical_book.bin',
+    #     './data/medical_encyclopedia.bin',
+    #     './data/wiki.bin',
+    #     './data/c4_zh_0.bin',
+    #     './data/c4_zh_1.bin',
+    #     './data/c4_zh_2.bin',
+    #     './data/c4_zh_3.bin',
+    #     './data/c4_zh_4.bin',
+    #     './data/c4_zh_5.bin',
+    #     './data/c4_zh_6.bin',
+    #     './data/c4_zh_7.bin',
+    #     './data/c4_zh_8.bin',
+    #     './data/wudaocorpus_zh_0.bin',
+    #     './data/wudaocorpus_zh_1.bin',
+    #     './data/wudaocorpus_zh_2.bin',
+    #     './data/wudaocorpus_zh_3.bin',
+    #     './data/wudaocorpus_zh_4.bin',
+    #     './data/wudaocorpus_zh_5.bin',
+    #     './data/wudaocorpus_zh_6.bin',
+    #     './data/wudaocorpus_zh_7.bin',
+    #     './data/wudaocorpus_zh_8.bin',
+    #     './data/wudaocorpus_zh_9.bin',
+    #     './data/wudaocorpus_zh_10.bin',
+    #     './data/wudaocorpus_zh_11.bin',
+    #     './data/wudaocorpus_zh_12.bin',
+    #     './data/wudaocorpus_zh_13.bin',
+    #     './data/wudaocorpus_zh_14.bin',
+    #     './data/wudaocorpus_zh_15.bin',
+    #     './data/wudaocorpus_zh_16.bin',
+    # ]
+    # data_lst=[]
+    # for data_path in tqdm(data_path_list):
+    #     with open(data_path,'rb') as f:
+    #         data=np.fromfile(f,dtype=np.uint16)
+    #         data_lst.append(data)
+    # arr = np.concatenate(data_lst)
+    # print(arr.shape)
+    # with open('./data/pretrain_data.bin','wb') as f:
+    #     f.write(arr.tobytes())
